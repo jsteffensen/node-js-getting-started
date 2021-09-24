fcdece5f12c4305f56e4d190bc9bec1487a233fc
jsteffensen <jsteffensen@users.noreply.github.com>
2021-09-24 20:31:28 +0200

    add postgres

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('trust proxy', true)
   .set('view engine', 'ejs')
-  .get('/', (req, res) => {
+  .get('/*', (req, res) => {
 	  res.render('pages/index');
 	  console.log('request ip -------------> ' + req.ips);
+	  
+		client.query('SELECT table_name FROM information_schema.tables;', (err, res) => {
+		  if (err) throw err;
+		  for (let row of res.rows) {
+			console.log(JSON.stringify(row));
+		  }
+		  client.end();
+		});
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
