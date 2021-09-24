aa4814d04a80e83a6f930ae87ebbb5a860b6b441
jsteffensen <jsteffensen@users.noreply.github.com>
2021-09-24 21:14:52 +0200

    add jawsDB Maria

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
 
 
 
@@ -17,14 +26,5 @@ express()
 	  res.render('pages/index');
 	  console.log('request ip -------------> ' + req.ips);
 	  
-		connection.connect();
-
-		connection.query('CREATE TABLE Ips (IpAddress varchar(255));', function(err, rows, fields) {
-		  if (err) throw err;
-
-		  console.log('The solution is: ', rows[0].solution);
-		});
-
-		connection.end();
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
